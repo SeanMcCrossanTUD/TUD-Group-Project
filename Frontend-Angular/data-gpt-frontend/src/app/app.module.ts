b056a32e2c0ea85c8219b6b19ab350d3ee856b52
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-10-05 12:15:25 +0100

    FAB-24. Angular UI update.

--- Frontend-Angular/data-gpt-frontend/src/app/app.module.ts
+++ Frontend-Angular/data-gpt-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend-Angular/data-gpt-frontend/src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.css']
+})
+export class UploadFileComponent {
+
+}
