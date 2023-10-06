74950601407132c0e7dee979bcf71123e8eb83e4
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-10-06 11:09:19 +0100

    FAB-24. Angular file upload service created.

--- Frontend-Angular/data-gpt-frontend/src/app/app.module.ts
+++ Frontend-Angular/data-gpt-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
@@ -12,7 +12,8 @@ import { UploadFileComponent } from './upload-file/upload-file.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
