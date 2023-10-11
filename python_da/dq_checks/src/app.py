51ced9e4cb8f37d8d25e3bbfbdef18dd60f5afe2
SeanMcCrossanTUD <D22124413@mytudublin.ie>
2023-10-12 00:31:09 +0100

    Implemented image upload to Azure Blob Storage - Added functionality to generate and upload data profiling visuals to Azure - Enhanced error handling for scenarios with empty or whitespace-only CSV data - Introduced fallback mechanism to download 'dirty_startup_dataset.csv' when required

--- python_da/dq_checks/src/app.py
+++ python_da/dq_checks/src/app.py
@@ -8,6 +8,8 @@ import pandas as pd
 from azure.core.exceptions import AzureError
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from .data_quality_checker import DataQualityChecker
+from .data_profiling_visuals import DataProfilingVisuals
+
 
 # Set up logging
 logger = logging.getLogger('data-quality-check')
@@ -25,7 +27,7 @@ if connection_string is None:
     raise Exception("Failed to get connection string from environment variable")
 
 container_name = "csv"
-
+container_name_images = "imagesoutput"
 @app.route('/health', methods=['GET'])
 def health_check():
     """
@@ -58,6 +60,27 @@ def download_blob_csv_data():
         Exception: For general exceptions that might occur while trying to download the blob data.
     """
 
+def download_blob_csv_data():
+    """
+    Downloads the CSV from Azure Blob Storage, converts it into a Pandas DataFrame, and returns it.
+
+    This function initializes a BlobServiceClient using the connection credentials, retrieves the blob client for a specific
+    container, and lists all blobs within that container. 
+    
+    FOR TESTING PURPOSES!!!!!If the downloaded CSV data is empty or contains only whitespace,
+    it will attempt to download 'dirty_startup_dataset.csv' instead. # CHANGE ASAP AFTER FRIDAY DEMO
+
+    Logging and error handling is utilized to provide information about the process
+    and to provide specific errors. 
+
+    Returns:
+        pd.DataFrame: A Pandas DataFrame containing the CSV data downloaded from Azure Blob Storage.
+
+    Raises:
+        AzureError: If there is an Azure-specific error while trying to download the blob data.
+        Exception: For general exceptions that might occur while trying to download the blob data.
+    """
+
     try:
         logger.info('Initializing BlobServiceClient')
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -65,10 +88,22 @@ def download_blob_csv_data():
         blob_list = container_client.list_blobs()
         blob_name = next(blob_list).name  # Assumes only one blob
 
+        if blob_name is None:  # Checking if any blobs in container.
+            logger.error('No blobs found in container')
+            raise Exception('No blobs found in the container')
+
         logger.info(f'Downloading CSV data from Azure Blob Storage: {blob_name}')
         blob_client = blob_service_client.get_blob_client(container_name, blob_name)
         blob_data = blob_client.download_blob()
         csv_data = blob_data.readall().decode('utf-8')
+
+        if not csv_data.strip(): 
+            logger.warning("Downloaded CSV data is empty or contains only whitespace. Attempting to download 'dirty_startup_dataset.csv' instead.")
+            blob_name = "dirty_startup_dataset.csv"
+            blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+            blob_data = blob_client.download_blob()
+            csv_data = blob_data.readall().decode('utf-8')
+
         data = pd.read_csv(io.StringIO(csv_data))
 
         return data
@@ -79,6 +114,7 @@ def download_blob_csv_data():
     except Exception as e:
         logger.error(f"Unexpected error while downloading blob data: {str(e)}")
         raise e 
+
 
 
 def perform_data_quality_checks(data):
@@ -154,18 +190,6 @@ def perform_data_quality_checks(data):
 
     return result
 
-def save_results_to_json_folder(result):
-    
-    result_path = "/Users/seanmccrossan/group_project/TUD-Group-Project/python-da/results"
-    # Create the directory if it does not exist
-    os.makedirs(result_path, exist_ok=True)
-    
-    # Define the name of the result file
-    result_file = os.path.join(result_path, 'data_quality_result.json')
-    
-    # Write the result into a JSON file
-    with open(result_file, 'w') as outfile:
-        json.dump(result, outfile, indent=4)
 
 def upload_results_to_azure(result):
     # Convert the result dictionary to a JSON string
@@ -198,6 +222,43 @@ def upload_results_to_azure(result):
         logger.error(f"Unexpected error while uploading result to Azure Blob Storage: {str(e)}")
         raise e 
 
+def upload_image_to_azure(img_data, blob_name, container_name_images, connection_string):
+    try:
+        # Initialize BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Get the blob client for the result
+        blob_client = blob_service_client.get_blob_client(container_name_images, blob_name)
+        
+        # Upload the image data to Azure Blob Storage
+        blob_client.upload_blob(img_data, overwrite=True)
+        print(f'Successfully uploaded {blob_name} to {container_name_images} in Azure Blob Storage')
+        return blob_name
+    
+    except Exception as e:
+        print(f"Error while uploading image to Azure Blob Storage: {str(e)}")
+        raise e
+
+def run_visuals_and_upload(data_quality_checker, connection_string, container_name_images):
+    visuals = DataProfilingVisuals(data_quality_checker)
+
+    # Define the methods to run and the corresponding blob names
+    methods_and_blob_names = [
+        (visuals.plot_missing_values, 'missing_values_plot'),
+        (visuals.plot_unique_values_in_text_fields, 'unique_values_plot'),
+        (visuals.plot_outlier_table, 'outlier_table'),
+        (visuals.plot_outlier_scatter, 'outlier_scatter')
+    ]
+
+    # Run methods and upload images to Azure
+    for method, blob_name_template in methods_and_blob_names:
+        img_data = method()
+        if img_data is not None:  # Check if the method returned image data
+            timestamp = str(int(time.time()))
+            blob_name = f'{blob_name_template}_{timestamp}.png'    
+            upload_image_to_azure(img_data, blob_name, container_name_images, connection_string)
+
+
 
 @app.route('/data-quality-check', methods=['GET'])
 def data_quality_check():
@@ -220,6 +281,33 @@ def data_quality_check():
     except Exception as e:
         logger.error(f"Unexpected error: {str(e)}")
         return jsonify({'error': str(e), 'type': str(type(e).__name__)}), 500
+
+@app.route('/data-profiling-images', methods=['GET'])
+def data_profiling_images():
+    try:
+        logger.info('Running Data Profiling Images module!')
+        
+        # Download CSV data from Azure Blob Storage and convert it to a Pandas DataFrame
+        data = download_blob_csv_data()
+        
+        # Initialize the DataQualityChecker with the downloaded data
+        data_quality_checker = DataQualityChecker(data)
+        
+        # Run visuals and upload images to Azure Blob Storage
+        run_visuals_and_upload(data_quality_checker, connection_string, container_name_images)
+        
+        return jsonify({'status': 'success', 'message': 'Data profiling images generated and uploaded successfully'}), 200
+
+    except AzureError as ae:
+        logger.error(f"AzureError: {str(ae)}")
+        return jsonify({'error': str(ae), 'type': 'AzureError'}), 500
+    except pd.errors.EmptyDataError as ede:
+        logger.error(f"Pandas EmptyDataError: {str(ede)}")
+        return jsonify({'error': str(ede), 'type': 'EmptyDataError'}), 500
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({'error': str(e), 'type': str(type(e).__name__)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
