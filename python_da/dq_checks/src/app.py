70e14d545250cc38cad87fd98587461e7b4d8153
SeanMcCrossanTUD <D22124413@mytudublin.ie>
2023-10-19 18:15:21 +0100

    FAB-205: Add configuration file
    - Added a configuration file to make refactoring code easier.
    - Applied it to data prep and dq-checks module
    - Fixed package imports in unit tests
    - Discovered bug in data prep unit tests, jira FAB-206

--- python_da/dq_checks/src/app.py
+++ python_da/dq_checks/src/app.py
@@ -25,14 +25,22 @@ logger.addHandler(handler)
 
 app = Flask(__name__)
 
-SERVICE_BUS_CONNECTION_STRING = 'Endpoint=sb://fab5-mq.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/i79GR2PUSm8IuWFqlgqCHP9BJ2+QYPm0+ASbDU8pRM='
-SERVICE_BUS_QUEUE_NAME = 'q1'
+# Load configuration from JSON file
+with open('python_da/pyconfigurations/azure_config.json', 'r') as file:
+    config = json.load(file)
 
-connection_string = os.getenv('AZURE_CONNECTION_STRING')
+SERVICE_BUS_CONNECTION_STRING = config["SERVICE_BUS_CONNECTION_STRING"]
+SERVICE_BUS_QUEUE_NAME = config["SERVICE_BUS_QUEUE_1_NAME"]
+connection_string = config["AZURE_CONNECTION_STRING"]
+
+
 if connection_string is None:
     raise Exception("Failed to get connection string from environment variable")
 
-container_name_images = "imagesoutput"
+container_name_images = config["IMAGE_OUTPUT_CONTAINER"]
+container_name_data_input = config["DATA_INPUT_CONTAINER"]
+
+
 
 @app.route('/health', methods=['GET'])
 def health_check():
@@ -143,7 +151,7 @@ def run_visuals_and_upload(data_quality_checker, connection_string, container_na
 def data_quality_check():
     try:
         logger.info('Running Data Profile module!')
-        data = download_blob_csv_data(connection_string=connection_string)
+        data = download_blob_csv_data(connection_string=connection_string, container_name=container_name_data_input)
         result = perform_data_quality_checks(data)
 
         # Upload the result to Azure Blob Storage
