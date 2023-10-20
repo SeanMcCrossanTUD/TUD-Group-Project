fe9e22f53bae6daa10401b81c19649beaafaa377
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-10-21 00:01:31 +0100

    Update DataCleaningController return type to Void

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/DataCleaningController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/DataCleaningController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.Data.PolishBackend.Service.DataCleaningService; // Import the FileUploadService
+import com.example.Data.PolishBackend.Service.DataCleaningService; // Import the DataCleaningService
 
 
 @RestController
@@ -17,7 +17,7 @@ public class DataCleaningController {
     private DataCleaningService dataCleaningService;
 
     @GetMapping("/data-clean")
-    public ResponseEntity<String> cleanData(@RequestParam String jobID) {
+    public ResponseEntity<Void> cleanData(@RequestParam String jobID) {
         return dataCleaningService.processDataCleaning(jobID);
     }
 }
