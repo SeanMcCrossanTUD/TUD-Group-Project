eaa9cbd73c0abdf6b135c0e1681b7eab2d33e455
SeanMcCrossanTUD <D22124413@mytudublin.ie>
2023-11-07 11:38:32 +0000

    FAB-247: Made change for naveen

--- python_da/dq_checks/run_main.py
+++ python_da/dq_checks/run_main.py
@@ -171,7 +171,7 @@ def main(test_iterations=None):
                 logger.info(f'Data profile images created and uploaded: {filename} - {jobID}')
 
                 logger.info(f'Data profile images nessage sent to service bus queue: {filename} - {jobID}')
-                time.sleep(600)
+                time.sleep(6000)
 
                 # Increment the counter
                 counter += 1
