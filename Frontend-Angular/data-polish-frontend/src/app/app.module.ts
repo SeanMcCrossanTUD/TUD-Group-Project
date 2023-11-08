aa41e989fc73d0f639d70875d5030da3561b0fd3
Nikodem Adamski <75024150+nikodemadamski@users.noreply.github.com>
2023-11-08 23:26:11 +0000

    cardinality chart added
    i was messing with our code and i have created a cardinality chart that is fully functional , the next step is creating teh other 3 graphs

--- Frontend-Angular/data-polish-frontend/src/app/app.module.ts
+++ Frontend-Angular/data-polish-frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import {CookieService} from 'ngx-cookie-service';
 import { TerminalModule, TerminalService } from 'primeng/terminal';
 import { DockModule } from 'primeng/dock';
 import { FormsModule } from '@angular/forms';
+import { CardinalityComponent } from './D3/cardinality/cardinality.component';
 
 const routes: Routes = [
     {path:'',component:HomePageComponent},
@@ -38,7 +39,8 @@ const routes: Routes = [
     DataProfileComponent,
     HomePageComponent,
     ExportComponent,
-    DataCleaningComponent
+    DataCleaningComponent,
+    CardinalityComponent
   ],
   imports: [
     BrowserModule,
