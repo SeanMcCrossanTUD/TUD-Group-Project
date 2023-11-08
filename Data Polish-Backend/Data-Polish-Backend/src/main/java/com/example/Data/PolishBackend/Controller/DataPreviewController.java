2c34de2e8ee360ecd753decca202eb5782e9f60c
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-11-08 13:33:18 +0000

    Created DataPreviewController - final version

---
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/DataPreviewController.java
@@ -0,0 +1,23 @@
+package com.example.Data.PolishBackend.Controller;
+
+import com.example.Data.PolishBackend.Service.DataCleaningService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.Data.PolishBackend.Service.DataPreviewService; // Import the DataCleaningService
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
+public class DataPreviewController {
+    @Autowired
+    private DataPreviewService dataPreviewService;
+
+    @GetMapping("/data-preview")
+    public ResponseEntity<String> previewData(@RequestParam String jobID) {
+        return dataPreviewService.processDataPreview(jobID);
+    }
+}
