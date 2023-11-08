cb1cc34647b1f1c0cff0b1dbd1d0ec2f8a07958a
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-11-08 12:27:11 +0000

    Created structure for DataPreviewService

---
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/DataPreviewService.java
@@ -0,0 +1,8 @@
+package com.example.Data.PolishBackend.Service;
+/*
+    take jobid as param
+    query db with jobid to get datapreview entry
+    return it to frontend
+ */
+public class DataPreviewService {
+}
