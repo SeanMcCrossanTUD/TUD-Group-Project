a2c95d2aa95ade5561ef8036068868e5c31ddcd4
SeanMcCrossanTUD <D22124413@mytudublin.ie>
2023-11-22 17:03:30 +0000

    FAB-287: Add legend

--- Frontend-Angular/data-polish-frontend/src/app/D3/histogram/histogram.component.ts
+++ Frontend-Angular/data-polish-frontend/src/app/D3/histogram/histogram.component.ts
@@ -79,6 +79,36 @@ export class HistogramComponent implements OnInit {
     svg.append("g")
       .call(d3.axisLeft(y));
 
+    // Legend setup
+    const legendData = [
+      { color: "#69b3a2", label: "Histogram" },
+      { color: "red", label: "Data Curve" },
+      { color: "green", label: "Ideal Normal" }
+    ];
+
+    const legend = svg.append("g")
+      .attr("class", "legend")
+      .attr("transform", `translate(${width - 150}, 20)`);
+
+    legend.selectAll("rect")
+      .data(legendData)
+      .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", (d, i) => i * 20)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", d => d.color);
+
+    legend.selectAll("text")
+      .data(legendData)
+      .enter()
+      .append("text")
+      .attr("x", 24)
+      .attr("y", (d, i) => i * 20 + 14)
+      .text(d => d.label);
+
+
     svg.selectAll("rect")
       .data(bins)
       .enter()
