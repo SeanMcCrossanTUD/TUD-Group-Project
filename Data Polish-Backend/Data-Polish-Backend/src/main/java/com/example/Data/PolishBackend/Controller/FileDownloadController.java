d08e908f9f7624449ecc85101e3c584138add004
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-11-23 12:27:55 +0000

    allowing "http://16.170.150.247:9000", "http://localhost:4200"

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/FileDownloadController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/FileDownloadController.java
@@ -11,7 +11,7 @@ import com.example.Data.PolishBackend.Service.FileDownloadService; // Import the
 
 
 @RestController
-@CrossOrigin(origins = {"*"})
+@CrossOrigin(origins = {"http://16.170.150.247:9000", "http://localhost:4200"})
 public class FileDownloadController {
     @Autowired
     private FileDownloadService fileDownloadService;
