8744cd86d1e38c408b512548e55002f7fbc16893
Nikodem Adamski <75024150+nikodemadamski@users.noreply.github.com>
2023-11-28 09:37:30 +0000

    initial v1 of the bubble chart
    it needs refinemnet but it is teh initail bubble chart, this took me wayyyy too long i will look fro an alternative to reduce production time , i spent until 2 am to get this working

--- Frontend-Angular/data-polish-frontend/src/app/D3/bubble-chart/bubble-chart.component.ts
+++ Frontend-Angular/data-polish-frontend/src/app/D3/bubble-chart/bubble-chart.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-bubble-chart',
   templateUrl: './bubble-chart.component.html',
   styleUrls: ['./bubble-chart.component.css']
 })
-export class BubbleChartComponent {
+export class BubbleChartComponent implements OnInit {
 
+  private data: any[] = [
+    { "id": 1, "category": "Category A", "value1": 10, "value2": 20 },
+    { "id": 2, "category": "Category B", "value1": 15, "value2": 25 },
+    { "id": 3, "category": "Category C", "value1": 20, "value2": 30 },
+    { "id": 4, "category": "Category D", "value1": 25, "value2": 35 }
+  ];
+  
+  private currentColumn: string = 'value1';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createBubbleChart();
+  }
+
+  private createBubbleChart(): void {
+    d3.select('#bubbleChart').selectAll('*').remove();
+
+    const svg = d3.select('#bubbleChart').append('svg')
+      .attr('width', 600)
+      .attr('height', 400);
+
+    // Create groups for each bubble
+    const bubbleGroup = svg.selectAll('.bubble')
+      .data(this.data)
+      .enter().append('g')
+      .attr('class', 'bubble')
+      .attr('transform', (d, i) => `translate(${100 + i * 100}, 200)`);
+
+    // Create circles
+    bubbleGroup.append('circle')
+      .attr('r', d => +d[this.currentColumn])
+      .attr('fill', 'steelblue');
+
+    // Create text labels
+    bubbleGroup.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.3em') // Center vertically
+      .text(d => d.category); // Replace 'd.category' with the text you want to display
+
+    // Add animation to circles
+    bubbleGroup.select('circle').transition()
+      .duration(750)
+      .attr('r', d => +d[this.currentColumn] * 1.5)
+      .transition()
+      .duration(750)
+      .attr('r', d => +d[this.currentColumn]);
+  }
+
+  setColumn(column: string): void {
+    this.currentColumn = column;
+    this.createBubbleChart();
+  }
 }
