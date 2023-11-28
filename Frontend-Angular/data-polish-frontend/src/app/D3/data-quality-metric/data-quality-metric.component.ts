1212449daa66fddec667558f06fc759ba5f2bc65
Nikodem Adamski <75024150+nikodemadamski@users.noreply.github.com>
2023-11-28 14:06:41 +0000

    Updated data quality metric by adding colour !
    I added
    - colour change for numbers above 80 - green , 60-80 orange , below 60 red!
    - changed font size of % to be better visible

--- Frontend-Angular/data-polish-frontend/src/app/D3/data-quality-metric/data-quality-metric.component.ts
+++ Frontend-Angular/data-polish-frontend/src/app/D3/data-quality-metric/data-quality-metric.component.ts
@@ -20,42 +20,43 @@ export class DataQualityMetricComponent implements OnInit {
     const width = 200;
     const height = 200;
     const thickness = 20;
-
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-
+  
+    const getColor = (value: number) => {
+      if (value > 80) return 'green';
+      else if (value > 60) return 'orange';
+      else return 'red';
+    };
+  
     const svg = d3.select("#dq-metric")
       .append('svg')
       .attr('class', 'pie')
       .attr('width', width)
       .attr('height', height);
-
+  
     const arc = d3.arc<d3.PieArcDatum<any>>()
       .innerRadius((width / 2) - thickness)
       .outerRadius(width / 2);
-
-    // Correct the pie value function
-    const pie = d3.pie<{value: number}>() // Specify the data type for the pie generator
-      .value(d => d.value); // Access the 'value' property of each data object
-
-
+  
+    const pie = d3.pie<{ value: number }>()
+      .value(d => d.value);
+  
     const path = svg.append('g')
       .attr('transform', `translate(${width / 2}, ${height / 2})`)
       .selectAll('path')
-      .data(pie([{value: dataset}, {value: 1 - dataset}])) // Correctly structured data for the pie generator
+      .data(pie([{ value: dataset }, { value: 1 - dataset }]))
       .enter()
       .append("path")
       .attr("d", arc)
-      .attr("fill", (d, i) => i === 0 ? color('0') : '#ddd')
+      .attr("fill", (d, i) => i === 0 ? getColor(this.data[0].value) : '#ddd')
       .attr("stroke", "white")
       .attr("stroke-width", "6");
-
-
+  
     const text = svg.append("text")
       .text(`${this.data[0].value}%`)
       .attr("text-anchor", "middle")
       .attr("dominant-baseline", "middle")
       .attr("transform", `translate(${width / 2}, ${height / 2})`)
-      .attr("font-size", "30")
-      .attr("fill", "#275478");
-  }
+      .attr("font-size", "40")
+      .attr("fill", getColor(this.data[0].value));
+  }  
 }
