11103eea3d9d82635d0808ed69d929aaad1c1ad9
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-11-29 00:07:43 +0000

    DataProfileController- added secret key and jwt authentication

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/DataProfileController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/DataProfileController.java
@@ -2,13 +2,14 @@ package com.example.Data.PolishBackend.Controller;
 
 
 import com.example.Data.PolishBackend.Service.DataProfileService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 
 @RestController
@@ -17,9 +18,51 @@ public class DataProfileController {
     @Autowired
     private DataProfileService dataProfileService;
 
+    // secure secret key 256 bits
+    private final String secretKey = "WaaZ5eNs94mGk+joiqJf6Laj1s0oOrAyoy/saZJAaom801Rpqy88IaDZGhkhd65e";
+
     @GetMapping("/data-profile")
-    public ResponseEntity<String> previewData(@RequestParam String jobID) {
+    /*public ResponseEntity<String> profileData(@RequestParam String jobID) {
         return dataProfileService.processDataProfile(jobID);
+    } */
+    public ResponseEntity<String> profileData(@RequestParam String jobID, @RequestHeader("Authorization") String token) {
+
+        // Validate the JWT token
+        if (isValidJwtToken(token)) {
+            return dataProfileService.processDataProfile(jobID);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+    }
+
+
+
+    private boolean isValidJwtToken(String token) {
+        try {
+            // Remove the "Bearer " prefix if present
+            if (token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
+            // Parse the token and extract claims
+            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+            // Log the claims for debugging
+            System.out.println("Token Claims: " + claims);
+
+            // Check if the token has expired
+            if (claims.getExpiration() != null && claims.getExpiration().before(new Date())) {
+                // Token has expired
+                System.out.println("Token has expired");
+                return false;
+            }
+
+            // If no exception is thrown, the token is valid
+            return true;
+        } catch (Exception e) {
+            // An exception is thrown if the token is invalid
+            System.out.println("Exception during token validation: " + e.getMessage());
+            return false;
+        }
     }
 }
 
