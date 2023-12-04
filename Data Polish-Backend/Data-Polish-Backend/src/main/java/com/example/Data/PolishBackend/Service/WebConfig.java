ce3914c242846098bd19e53c87fa8c2b4d47ebd6
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-12-04 22:34:05 +0000

    globaly cors fix

---
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.Data.PolishBackend.Service;
+
+
+import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.CorsRegistry;
+        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://16.170.150.247:9000", "http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
