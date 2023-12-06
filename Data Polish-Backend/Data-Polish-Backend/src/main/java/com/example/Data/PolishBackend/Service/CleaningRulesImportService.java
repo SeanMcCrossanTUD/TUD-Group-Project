cdc6e4416e18e098656ec99d0d014203a148adb0
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 17:02:12 +0000

    Created and added requiredments for CleaningRulesImportService

---
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
@@ -0,0 +1,8 @@
+package com.example.Data.PolishBackend.Service;
+/*
+* take jobid as param, json file (multipart file like upload)
+* upload this json file into blob container 'rules'
+* update db with the URI in column 'cleaningrules'
+* */
+public class CleaningRulesImportService {
+}
