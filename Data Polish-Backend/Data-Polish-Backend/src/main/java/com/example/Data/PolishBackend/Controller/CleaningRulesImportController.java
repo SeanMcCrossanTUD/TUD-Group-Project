ed8ad13c56191c5a5d2d0ebd390a9f1bed9c681d
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 20:27:37 +0000

    added entire importcontroller code

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
@@ -1,12 +1,24 @@
 package com.example.Data.PolishBackend.Controller;
 
+import com.example.Data.PolishBackend.Service.CleaningRulesImportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RestController
 public class CleaningRulesImportController {
+    @Autowired
+    private CleaningRulesImportService cleaningRulesImportService;
+
+    @PostMapping("/rules-import")
+    public ResponseEntity<String> importCleaningRules(
+            @RequestParam String jobID,
+            @RequestParam("file") MultipartFile jsonFile,
+            @RequestHeader("Authorization") String token
+    ) throws IOException  {
+        return cleaningRulesImportService.importCleaningRules(jobID, jsonFile);
+    }
 }
