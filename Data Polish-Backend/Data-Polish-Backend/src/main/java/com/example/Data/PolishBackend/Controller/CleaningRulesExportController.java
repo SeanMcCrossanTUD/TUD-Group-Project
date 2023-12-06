52b194b62a3a66d18ff27384a54110e32493d4da
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:38:14 +0000

    added authentication for exportcontroller

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesExportController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesExportController.java
@@ -1,20 +1,58 @@
 package com.example.Data.PolishBackend.Controller;
 
 import com.example.Data.PolishBackend.Service.CleaningRulesExportService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 public class CleaningRulesExportController {
     @Autowired
     private CleaningRulesExportService cleaningRulesExportService;
-    
+
     // secure secret key 256 bits
     private final String secretKey = "WaaZ5eNs94mGk+joiqJf6Laj1s0oOrAyoy/saZJAaom801Rpqy88IaDZGhkhd65e";
 
     @GetMapping("/rules-export")
-    public ResponseEntity<String> exportCleaningRules(@RequestParam String jobID) {
-        return cleaningRulesExportService.exportCleaningRules(jobID);
+    public ResponseEntity<String> exportCleaningRules(@RequestParam String jobID, @RequestHeader("Authorization") String token) {
+        // Validate the JWT token
+        if (isValidJwtToken(token)) {
+            return cleaningRulesExportService.exportCleaningRules(jobID);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+    }
+
+    private boolean isValidJwtToken(String token) {
+        try {
+            // Remove the "Bearer " prefix if present
+            if (token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
+            // Parse the token and extract claims
+            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+            // Log the claims for debugging
+            System.out.println("Token Claims: " + claims);
+
+            // Check if the token has expired
+            if (claims.getExpiration() != null && claims.getExpiration().before(new Date())) {
+                // Token has expired
+                System.out.println("Token has expired");
+                return false;
+            }
+
+            // If no exception is thrown, the token is valid
+            return true;
+        } catch (Exception e) {
+            // An exception is thrown if the token is invalid
+            System.out.println("Exception during token validation: " + e.getMessage());
+            return false;
+        }
     }
 }
