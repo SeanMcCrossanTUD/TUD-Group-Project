c9910957bd0ebbade1f01b98ca350d6353f66540
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 20:17:07 +0000

    added azure storage connection string

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
@@ -1,24 +1,55 @@
 package com.example.Data.PolishBackend.Service;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.core.io.Resource;
+
 import java.util.UUID;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Objects;
 
 /*
 * take jobid as param, json file (multipart file like upload) from frontend
 * upload this json file into blob container 'rules', name of the blob? same as cleaning
 * update db with the URI(this new file's name) in column 'cleaningrules'
 * */
+
+@Service
 public class CleaningRulesImportService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    //storing the blob connection string in a 'string' variable
+    @Value("${azure.storage.connection-string}")
+    private String azureConnectionString;
+
+
+    public ResponseEntity<String> importCleaningRules(String jobID, MultipartFile jsonFile) {
+        try {
+
+            // Generate a unique filename for the JSON file
+            String FileName = "rules_" + UUID.randomUUID() + ".json";
+
+            // Store the JSON file to the 'rules' blob container
+            storeJsonInBlobStorage(jsonFile, FileName);
+
+            // Update the 'cleaningrules' column in the 'jobsandblobs' table
+            String sql = "UPDATE jobsandblobs SET cleaningrules = ? WHERE jobid = ?";
+            jdbcTemplate.update(sql, FileName, jobID);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred during file upload: " + e.getMessage());
+        }
+    }
+
+
 }
+
