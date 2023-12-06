afa8c853b69c93bb36150eab61ca1d10cad9c36d
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 20:22:49 +0000

    added imports to importcontroller

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
@@ -1,4 +1,12 @@
 package com.example.Data.PolishBackend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 public class CleaningRulesImportController {
 }
