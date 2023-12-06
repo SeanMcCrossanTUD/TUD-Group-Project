98ee6743a03d65b3fbad78695d2b2412cd3a3ffe
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 12:11:21 +0000

    removed unnecessary code

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/FileDownloadService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/FileDownloadService.java
@@ -1,17 +1,21 @@
 package com.example.Data.PolishBackend.Service;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -20,9 +24,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.*;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+
+
 /*
     take jobid as param
     query db to get file details (every column)
@@ -54,34 +81,47 @@ public class FileDownloadService {
             String blobUrl = String.format("https://fab5storage.blob.core.windows.net/output/%s", cleanedFile);
 
             // Download the file using HTTP
-            byte[] fileContent = downloadBlob(blobUrl);
+            Resource download = downloadBlob(blobUrl);
 
             // Extract file extension from cleanedFile
             String fileExtension = cleanedFile.substring(cleanedFile.lastIndexOf('.'));
 
-            // If the file extension matches the requested fileType, return the file
+            //check if there is a match then just return as it is
             if (fileExtension.equals(fileType)) {
-                ByteArrayResource resource = new ByteArrayResource(fileContent);
-                return ResponseEntity.ok()
-                        .header("Content-Disposition", "attachment;filename=" + cleanedFile)
-                        .body(resource);
-            } else {
-                // Convert the file to the requested fileType
-                byte[] convertedFileContent;
-                if (fileType.equals(".csv")) {
-                    convertedFileContent = convertCsvToXlsx(fileContent);
-                } else if (fileType.equals(".xlsx")) {
-                    convertedFileContent = convertXlsxToCsv(fileContent);
-                } else {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-                }
-
-                String convertedFileName = cleanedFile.replace(fileExtension, fileType);
-                ByteArrayResource resource = new ByteArrayResource(convertedFileContent);
-                return ResponseEntity.ok()
-                        .header("Content-Disposition", "attachment;filename=" + convertedFileName)
-                        .body(resource);
+                // If they match, return the file as-is
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + cleanedFile);
+                return new ResponseEntity<>(download, headers, HttpStatus.OK);
             }
+
+            // If the file extension doesn't match the specified fileType, perform conversion
+            else if (".csv".equals(fileType) && ".xlsx".equals(fileExtension)) {
+                // Convert XLSX to CSV
+                String convertedContent = convertXlsxToCsv(download.getInputStream());
+                ByteArrayResource convertedFile = new ByteArrayResource(convertedContent.getBytes(StandardCharsets.UTF_8));
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_TYPE, "text/csv");
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + cleanedFile + ".csv");
+
+                return new ResponseEntity<>(convertedFile, headers, HttpStatus.OK);
+            } else if (".xlsx".equals(fileType) && ".csv".equals(fileExtension)) {
+                // Convert CSV to XLSX
+                Workbook workbook = convertCsvToXlsx(download.getInputStream());
+                ByteArrayResource convertedFile = workbookToByteArrayResource(workbook);
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_TYPE, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + cleanedFile + ".xlsx");
+
+                return new ResponseEntity<>(convertedFile, headers, HttpStatus.OK);
+            } else {
+                // Unsupported conversion, return as-is
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + cleanedFile);
+                return new ResponseEntity<>(download, headers, HttpStatus.OK);
+            }
+
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
@@ -108,55 +148,74 @@ public class FileDownloadService {
 
          */
     }
-    private byte[] downloadBlob(String blobUrl) throws IOException {
+
+    private Resource downloadBlob(String blobUrl) throws IOException {
         try {
             URL url = new URL(blobUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
-            try (InputStream inputStream = connection.getInputStream()) {
-                return inputStream.readAllBytes();
-            }
+            // Get the input stream from the connection
+            InputStream inputStream = connection.getInputStream();
+
+            // Wrap the input stream in an InputStreamResource
+            return new InputStreamResource(inputStream);
         } catch (IOException e) {
             throw e;
         }
     }
 
-    // method to convert CSV to XLSX
-    private byte[] convertCsvToXlsx(byte[] csvContent) {
-        try {
-            // Parse CSV content to JsonNode
-            JsonNode jsonNode = csvMapper.readerFor(JsonNode.class)
-                    .with(CsvSchema.emptySchema().withHeader())
-                    .readTree(new ByteArrayInputStream(csvContent));
+    private Workbook convertCsvToXlsx(InputStream inputStream) throws IOException {
+        // Implement CSV to XLSX conversion logic using Apache POI
+        // This is a simplified example, you may need to adapt it based on your CSV format
+        // Note: The CSV data is assumed to be in a single sheet with simple data in this example
 
-            // Convert JsonNode to XLSX bytes using ObjectMapper
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.writeValue(outputStream, jsonNode);
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Sheet1");
 
-            return outputStream.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return csvContent;
+        try (CSVParser csvParser = new CSVParser(new InputStreamReader(inputStream, StandardCharsets.UTF_8),
+                CSVFormat.DEFAULT.withHeader())) {
+            for (CSVRecord record : csvParser) {
+                Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
+                for (int i = 0; i < record.size(); i++) {
+                    Cell cell = row.createCell(i);
+                    cell.setCellValue(record.get(i));
+                }
+            }
+        }
+
+        return workbook;
+    }
+
+    private String convertXlsxToCsv(InputStream inputStream) throws IOException {
+        // Implement XLSX to CSV conversion logic using Apache POI
+        // This is a simplified example, you may need to adapt it based on your XLSX format
+        // Note: The CSV data is assumed to be a simple concatenation of values in this example
+
+        try (Workbook workbook = WorkbookFactory.create(inputStream)) {
+            Sheet sheet = workbook.getSheetAt(0);
+            StringWriter stringWriter = new StringWriter();
+            CSVPrinter csvPrinter = new CSVPrinter(stringWriter, CSVFormat.DEFAULT);
+
+            for (Row row : sheet) {
+                List<String> record = new ArrayList<>();
+                for (Cell cell : row) {
+                    record.add(cell.toString());
+                }
+                csvPrinter.printRecord(record);
+            }
+
+            return stringWriter.toString();
         }
     }
 
-    // method to convert XLSX to CSV
-    private byte[] convertXlsxToCsv(byte[] xlsxContent) {
-        try {
-            // Parse XLSX content to JsonNode using ObjectMapper
-            // Add your logic here if needed
+    private ByteArrayResource workbookToByteArrayResource(Workbook workbook) throws IOException {
+        // Convert Workbook to a byte array
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        workbook.write(byteArrayOutputStream);
 
-            // Convert JsonNode to CSV bytes using CsvMapper
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            // For now, just return the original content
-            outputStream.write(xlsxContent);
-            return outputStream.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return xlsxContent;
-        }
+        // Create a ByteArrayResource from the byte array
+        return new ByteArrayResource(byteArrayOutputStream.toByteArray());
     }
 }
 
