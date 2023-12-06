7ede921a8b98a8df43700c737046510d59b74991
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 20:29:50 +0000

    added authentication

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesImportController.java
@@ -1,12 +1,16 @@
 package com.example.Data.PolishBackend.Controller;
 
 import com.example.Data.PolishBackend.Service.CleaningRulesImportService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Date;
 
 @RestController
 public class CleaningRulesImportController {
@@ -22,6 +26,39 @@ public class CleaningRulesImportController {
             @RequestParam("file") MultipartFile jsonFile,
             @RequestHeader("Authorization") String token
     ) throws IOException  {
-        return cleaningRulesImportService.importCleaningRules(jobID, jsonFile);
+        // Validate the JWT token
+        if (isValidJwtToken(token)) {
+            return cleaningRulesImportService.importCleaningRules(jobID, jsonFile);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+    }
+
+    private boolean isValidJwtToken(String token) {
+        try {
+            // Remove the "Bearer " prefix if present
+            if (token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
+            // Parse the token and extract claims
+            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+            // Log the claims for debugging
+            System.out.println("Token Claims: " + claims);
+
+            // Check if the token has expired
+            if (claims.getExpiration() != null && claims.getExpiration().before(new Date())) {
+                // Token has expired
+                System.out.println("Token has expired");
+                return false;
+            }
+
+            // If no exception is thrown, the token is valid
+            return true;
+        } catch (Exception e) {
+            // An exception is thrown if the token is invalid
+            System.out.println("Exception during token validation: " + e.getMessage());
+            return false;
+        }
     }
 }
