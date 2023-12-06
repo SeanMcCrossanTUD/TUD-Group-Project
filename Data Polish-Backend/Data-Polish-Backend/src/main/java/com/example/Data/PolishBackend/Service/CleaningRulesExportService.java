22ee5b75a3ccb3477994986cde0b890bba4048cb
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:23:36 +0000

    added imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
@@ -1,4 +1,10 @@
 package com.example.Data.PolishBackend.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 /*
  * take jobid as param from frontend
  * query db with jobid and get value in column 'cleaningrules'
