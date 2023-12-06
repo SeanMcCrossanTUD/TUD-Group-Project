5d678c41c07fa9e730efc03427cd57bbcec10402
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:18:37 +0000

    created rulesexportservice class requirement

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
@@ -1,4 +1,8 @@
 package com.example.Data.PolishBackend.Service;
-
+/*
+ * take jobid as param from frontend
+ * query db with jobid and get value in column 'cleaningrules'
+ * return the value
+ * */
 public class CleaningRulesExportService {
 }
