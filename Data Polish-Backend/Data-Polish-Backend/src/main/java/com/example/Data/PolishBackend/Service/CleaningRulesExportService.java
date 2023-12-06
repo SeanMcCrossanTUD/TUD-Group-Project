a84cda26a98868fad80e900b013b7e891afed0cc
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:28:09 +0000

    updated CleaningRulesExportService entire code

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesExportService.java
@@ -10,5 +10,28 @@ import org.springframework.http.HttpStatus;
  * query db with jobid and get value in column 'cleaningrules'
  * return the value
  * */
+@Service
 public class CleaningRulesExportService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public ResponseEntity<String> exportCleaningRules(String jobID) {
+        try {
+            // Query the database to get the cleaning rules for the specified jobID
+            String sql = "SELECT cleaningrules FROM jobsandblobs WHERE jobid = ?";
+            String cleaningRules = jdbcTemplate.queryForObject(sql, String.class, jobID);
+
+            // Check if cleaningrules exists
+            if (cleaningRules != null) {
+                return ResponseEntity.ok(cleaningRules);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cleaning rules not found for the given jobID.");
+            }
+
+        } catch (Exception e) {
+            // Return an internal server error response if an exception occurs
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body();
+        }
+    }
 }
