8cb8f82ce9c74d6b49aef4a76da8a269d365f7bb
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:36:18 +0000

    updated code for exportcontroller

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesExportController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/CleaningRulesExportController.java
@@ -3,9 +3,15 @@ package com.example.Data.PolishBackend.Controller;
 import com.example.Data.PolishBackend.Service.CleaningRulesExportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CleaningRulesExportController {
+    @Autowired
+    private CleaningRulesExportService cleaningRulesExportService;
+
+    @GetMapping("/rules-export")
+    public ResponseEntity<String> exportCleaningRules(@RequestParam String jobID) {
+        return cleaningRulesExportService.exportCleaningRules(jobID);
+    }
 }
