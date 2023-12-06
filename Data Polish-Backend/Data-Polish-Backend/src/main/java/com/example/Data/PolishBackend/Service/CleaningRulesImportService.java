ea57d453c7ac7b8b3e3df9d018bf8791f5e58853
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:18:05 +0000

    updated service class requirement

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
@@ -1,8 +1,8 @@
 package com.example.Data.PolishBackend.Service;
 /*
-* take jobid as param, json file (multipart file like upload)
-* upload this json file into blob container 'rules'
-* update db with the URI in column 'cleaningrules'
+* take jobid as param, json file (multipart file like upload) from frontend
+* upload this json file into blob container 'rules', name of the blob? same as cleaning
+* update db with the URI(this new file's name) in column 'cleaningrules'
 * */
 public class CleaningRulesImportService {
 }
