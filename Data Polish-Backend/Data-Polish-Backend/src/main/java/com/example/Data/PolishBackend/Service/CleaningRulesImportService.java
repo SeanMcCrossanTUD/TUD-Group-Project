90da11a58f4ba57331b030fa758da72438b337de
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-06 19:48:25 +0000

    added imports to rulesimport

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/CleaningRulesImportService.java
@@ -1,4 +1,20 @@
 package com.example.Data.PolishBackend.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
+import java.util.UUID;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
 /*
 * take jobid as param, json file (multipart file like upload) from frontend
 * upload this json file into blob container 'rules', name of the blob? same as cleaning
