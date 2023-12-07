207e4ab845fd83a425a22e2eb42d9e7de214c1d0
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 16:51:25 +0000

    removed autowired

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
@@ -3,7 +3,6 @@ package com.example.Data.PolishBackend.Controller;
 import com.example.Data.PolishBackend.Service.TriggerCleaningService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
