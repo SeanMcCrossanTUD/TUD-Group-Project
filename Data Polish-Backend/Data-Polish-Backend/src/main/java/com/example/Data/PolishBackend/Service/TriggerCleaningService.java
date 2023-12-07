97bc31e069e717b4f1d68c18794fd329150da062
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 16:40:12 +0000

    removed unnecessary imports and few things

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
@@ -1,10 +1,5 @@
 package com.example.Data.PolishBackend.Service;
 
-import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,13 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.azure.messaging.servicebus.*;
 import com.azure.core.util.BinaryData;
 import org.springframework.stereotype.Service;
-import com.fasterxml.jackson.databind.JsonNode; //Java doesn't have a built-in JSON object type, so need to use a library like Jackson to work with JSON objects.
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.UUID;
 
 /*
 * take jobID from frontend
@@ -104,6 +92,4 @@ public class TriggerCleaningService {
             e.printStackTrace();
         }
     }
-
-
 }
