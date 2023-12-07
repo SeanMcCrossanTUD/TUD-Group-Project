9204c24303b15ab667101aaaa170ec3074c5cf08
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 16:40:21 +0000

    added controller imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
@@ -1,4 +1,13 @@
 package com.example.Data.PolishBackend.Controller;
 
+import com.example.Data.PolishBackend.Service.TriggerCleaningService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 public class TriggerCleaningController {
 }
