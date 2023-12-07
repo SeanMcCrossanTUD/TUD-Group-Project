21af15b0ca00de56081057ccbc098365d64f38ef
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 16:46:57 +0000

    updated paramete with token header

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/TriggerCleaningController.java
@@ -4,21 +4,18 @@ import com.example.Data.PolishBackend.Service.TriggerCleaningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TriggerCleaningController {
 
     private TriggerCleaningService triggerCleaningService;
-    
+
     // secure secret key 256 bits
     private final String secretKey = "WaaZ5eNs94mGk+joiqJf6Laj1s0oOrAyoy/saZJAaom801Rpqy88IaDZGhkhd65e";
 
     @PostMapping("/trigger-cleaning")
-    public ResponseEntity<String> triggerCleaning(@RequestParam String jobID) {
+    public ResponseEntity<String> triggerCleaning(@RequestParam String jobID, @RequestHeader("Authorization") String token) {
         return triggerCleaningService.triggerCleaning(jobID);
     }
 }
