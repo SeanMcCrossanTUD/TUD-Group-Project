3b7e567de57bc9bcd126f3922b49c15f1d5b5c89
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 15:16:03 +0000

    added requirements in comments

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
@@ -1,4 +1,9 @@
 package com.example.Data.PolishBackend.Service;
 
+/*
+* take jobID from frontend
+* query DB with jobID and get all details
+* publish message with JobID and file details in queue 2
+* */
 public class TriggerCleaningService {
 }
