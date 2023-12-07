c882b56b944e3ed33bd191f84028df7292e57187
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-07 15:19:07 +0000

    added imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/TriggerCleaningService.java
@@ -1,5 +1,28 @@
 package com.example.Data.PolishBackend.Service;
 
+package com.example.Data.PolishBackend.Service;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.azure.messaging.servicebus.*;
+import com.azure.core.util.BinaryData;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.JsonNode; //Java doesn't have a built-in JSON object type, so need to use a library like Jackson to work with JSON objects.
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
+
 /*
 * take jobID from frontend
 * query DB with jobID and get all details
