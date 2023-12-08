f1b0e7817b0d11255289928f1e21ecfbc38beb60
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-08 22:24:51 +0000

    basic app.ts

--- Frontend-Angular/landing-page/src/app/app.component.ts
+++ Frontend-Angular/landing-page/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'landing-page';
+  openTutorial() {
+    console.log('Opening tutorial...');
+    // Add logic to navigate to the tutorial page or show a modal
+  }
+
+  getStarted() {
+    console.log('Getting started...');
+    // Add logic to navigate to the get started page or perform other actions
+  }
 }
