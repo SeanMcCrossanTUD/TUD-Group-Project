eeeff2462d21aafb22311d57ce5044652e1282c2
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-12-10 22:22:35 +0000

    user reg update

--- Frontend-Angular/landing-page/src/app/user-registaration/user-registaration.component.ts
+++ Frontend-Angular/landing-page/src/app/user-registaration/user-registaration.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-registaration', 
   templateUrl: './user-registaration.component.html',
@@ -8,15 +9,20 @@ import { MessageService } from 'primeng/api';
 })
 export class UserRegistarationComponent {
   constructor(private http:HttpClient,
-      private messageService:MessageService
+      private messageService:MessageService,
+      private router:Router
     ){
+  }
+
+  goHome(){
+    this.router.navigate(["/"]);
   }
   ngOnInit(){
     
   }
   userName:any;
   email:any;
-  
+  showMessage=true;
   onSubmit(){
   
     var regex=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -39,27 +45,7 @@ export class UserRegistarationComponent {
 
       
 
-    // this.http.post(
-    //   'http://localhost:8090/user-register',{
-    //     "fullName":this.userName,
-    //     "email":this.email
-
-    //   }
-    // ).subscribe(
-    //   (res1:any)=>{
-    //     this.http
-    // .post('https://prod-46.northeurope.logic.azure.com:443/workflows/643dcc1efa6a41908dd2846b226a0ffd/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9LE2z3sTd7ucTVWRYu2K-TuRPi1MT1pk_LLR5KSjIo8',{
-    //   'EMAILID':this.email,
-    //   'OTP':res1
-    // }).subscribe(
-    //   (res)=>{
-    //     console.log(res);
-    //   }
-    // )
-    //   },(erroe1)=>{
-    //     console.log(erroe1)
-    //   }
-    // )
+  
 
 
  
@@ -75,7 +61,7 @@ export class UserRegistarationComponent {
             'OTP': otp
           }).subscribe(
             (res) => {
-              
+              this.showMessage=true;
             },
             (error) => {
               this.messageService.add({ severity: 'error', summary: 'Check your details', detail: 'something went wrong' });
@@ -87,7 +73,7 @@ export class UserRegistarationComponent {
         }
       );
     }
-
+    
       
 
   
