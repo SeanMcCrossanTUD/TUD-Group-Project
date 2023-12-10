3e25f8100b5831915864f78ba2dcb866ab08a3e6
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 23:19:37 +0000

    add imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/ForgetPasswordController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/ForgetPasswordController.java
@@ -1,4 +1,8 @@
 package com.example.Data.PolishBackend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class ForgetPasswordController {
 }
