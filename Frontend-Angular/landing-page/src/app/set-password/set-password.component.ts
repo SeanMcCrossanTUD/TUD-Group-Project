eec5c339c0544123c5b86657454e2e7ab69a2662
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-12-10 21:33:52 +0000

    created set password

---
+++ Frontend-Angular/landing-page/src/app/set-password/set-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-password', 
+  templateUrl: './set-password.component.html',
+  styleUrl: './set-password.component.css'
+})
+export class SetPasswordComponent {
+
+}
