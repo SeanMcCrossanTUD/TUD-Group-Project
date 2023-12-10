dd24e541cdb58fdb51e3a17fe202bc1a945d3e25
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-12-10 17:47:33 +0000

    navigation

--- Frontend-Angular/landing-page/src/app/landing-page/landing-page.component.ts
+++ Frontend-Angular/landing-page/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
@@ -8,13 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+ 
+  constructor(private router:Router){}
   openTutorial() {
     console.log('Opening tutorial...');
     // Add logic to navigate to the tutorial page or show a modal
   }
 
   getStarted() {
-    console.log('Getting started...');
+    var url=window.location+'user-registration'
+    window.location.replace(url);
     // Add logic to navigate to the get started page or perform other actions
+  }
+  navigateToAnotherPage() {
+   
   }
 }
