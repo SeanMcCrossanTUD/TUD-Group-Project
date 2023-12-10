56143176a1162915a23dd6964fd9e06cb908bd5e
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 23:21:28 +0000

    entire controller code

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/ForgetPasswordController.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Controller/ForgetPasswordController.java
@@ -1,8 +1,20 @@
 package com.example.Data.PolishBackend.Controller;
 
+import com.example.Data.PolishBackend.Service.ForgetPasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@CrossOrigin(origins = {"http://16.170.150.247:9000", "http://localhost:4200"})
+
 public class ForgetPasswordController {
+
+    @Autowired
+    private ForgetPasswordService forgetPasswordService;
+
+    @GetMapping("/forget-password")
+    public ResponseEntity<String> registerUser(@RequestParam String email) {
+        return forgetPasswordService.forgetPassword(email);
+    }
 }
