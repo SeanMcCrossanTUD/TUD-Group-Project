4c8e9a27d7d2ed9ef9f9f2606f71b1c76be7c1ca
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 17:40:24 +0000

    add imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/UserRegistrationService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/UserRegistrationService.java
@@ -2,11 +2,13 @@ package com.example.Data.PolishBackend.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import org.springframework.web.client.RestTemplate;
 
