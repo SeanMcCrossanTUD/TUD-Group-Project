959b74ce736f12eebd9f6518142f31b272f04c7e
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 23:09:20 +0000

    add imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/ForgetPasswordService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/ForgetPasswordService.java
@@ -1,4 +1,27 @@
 package com.example.Data.PolishBackend.Service;
 
+import com.azure.core.util.BinaryData;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.sun.net.httpserver.HttpsParameters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+/*
+* take email from frontend
+* generate a random 4 digit otp
+* encode the otp and insert into 'users' table for the email
+* return the unencrypted OTP
+*  */
+
 public class ForgetPasswordService {
 }
