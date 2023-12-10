d9da6c17fc844ba6895ef85b597007921796c2a2
D22124491-NaveenMaheswaran <D22124491@mytudublin.ie>
2023-12-10 22:06:59 +0000

    set passwprd

--- Frontend-Angular/landing-page/src/app/set-password/set-password.component.ts
+++ Frontend-Angular/landing-page/src/app/set-password/set-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-set-password', 
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './set-password.component.css'
 })
 export class SetPasswordComponent {
+  constructor( private messageService:MessageService){}
 
+  email:any;
+  otp:any;
+  password1:any;
+  password2:any;
+  onSubmit(){
+    if(!(this.password1!==this.password2 && this.password1!=null && this.password1 !==undefined)){
+      this.messageService.add({ severity: 'error', summary: 'Check your password', detail: 'make sure to enter same passwords in both box' });
+      return;
+    }
+    if(!(this.otp!=null && this.otp!=undefined && this.email!=null &&  this.email!=undefined)){
+      this.messageService.add({ severity: 'error', summary: 'Check your details', detail: 'make sure to enter all details correctly' });
+      return;
+    }
+
+    console.log()
+
+  }
 }
