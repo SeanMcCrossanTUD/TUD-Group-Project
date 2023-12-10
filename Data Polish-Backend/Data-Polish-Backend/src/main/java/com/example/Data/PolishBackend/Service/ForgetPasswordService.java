1d0eb5c85382f5ca745746ef8d5a244a89fe9428
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 23:12:34 +0000

    add encoder

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/ForgetPasswordService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/ForgetPasswordService.java
@@ -1,21 +1,15 @@
 package com.example.Data.PolishBackend.Service;
 
-import com.azure.core.util.BinaryData;
-import com.azure.messaging.servicebus.ServiceBusClientBuilder;
-import com.azure.messaging.servicebus.ServiceBusMessage;
-import com.azure.messaging.servicebus.ServiceBusSenderClient;
-import com.sun.net.httpserver.HttpsParameters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
+
 /*
 * take email from frontend
 * generate a random 4 digit otp
@@ -23,5 +17,11 @@ import java.util.Random;
 * return the unencrypted OTP
 *  */
 
+@Service
 public class ForgetPasswordService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 }
