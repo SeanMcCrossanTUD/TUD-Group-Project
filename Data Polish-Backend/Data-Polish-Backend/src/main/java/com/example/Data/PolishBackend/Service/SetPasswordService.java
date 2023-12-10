08557f877c77431f6fd981ab657a5e13514eed84
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 22:04:06 +0000

    add requirements

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/SetPasswordService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/SetPasswordService.java
@@ -1,4 +1,12 @@
 package com.example.Data.PolishBackend.Service;
 
+/*
+* take strings email, otp, password from frontend
+* get value from 'password' column from 'users' table for the email and store it as string 'encodedOTP'
+* decode the 'encodedOTP' value and match with otp from frontend
+* if they match then hash the password taken from frontend and insert hashed password into 'password' column
+* else return "OTP is invalid"
+* */
+
 public class SetPasswordService {
 }
