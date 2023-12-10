c7d59529036e32964a73b88d9e80aafd8c0cb264
UmamaSumlinTasnuva <d22124465@mytudublin.ie>
2023-12-10 22:06:38 +0000

    add imports

--- Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/SetPasswordService.java
+++ Data Polish-Backend/Data-Polish-Backend/src/main/java/com/example/Data/PolishBackend/Service/SetPasswordService.java
@@ -1,5 +1,13 @@
 package com.example.Data.PolishBackend.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
 /*
 * take strings email, otp, password from frontend
 * get value from 'password' column from 'users' table for the email and store it as string 'encodedOTP'
@@ -8,5 +16,20 @@ package com.example.Data.PolishBackend.Service;
 * else return "OTP is invalid"
 * */
 
+@Service
 public class SetPasswordService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public ResponseEntity<String> setPassword(String email, String otp, String newPassword) {
+        try {
+            // Retrieve the 'encodedOTP' value from the 'users' table for the given email
+            String encodedOTP = jdbcTemplate.queryForObject(
+                    "SELECT password FROM users WHERE email = ?",
+                    new Object[]{email},
+                    String.class
+            );
+        }
 }
